1144e6e7a7825e1b385b820390a30b4545103bfb
Aaron Nicholl <aaronnicholl@ymail.com>
2018-04-02 11:19:12 +0100

    New updates 2nd April

--- app/src/main/java/com/example/aaron/inthehole/HandicapAdjustment.java
+++ app/src/main/java/com/example/aaron/inthehole/HandicapAdjustment.java
@@ -102,7 +102,13 @@ public class HandicapAdjustment extends AppCompatActivity implements View.OnClic
                 double num1, num2,num3,num4,num5,num6,num7,num8,num9,num10,num11,num12,num13,num14,num15,num16,num17,num18;
                 num1 = Double.parseDouble(Handicap.getText().toString());
                 num2 = Double.parseDouble(Net.getText().toString());
-                if(num2 >76)
+                if(num2 >76 && num1>=36)
+                {
+                    double sum;
+                    sum = num1;
+                    adjust.setText(Double.toString(sum));
+                }
+                else if(num2 >76 && num1<36)
                 {
                 double sum;
                 sum = num1 +0.1;
