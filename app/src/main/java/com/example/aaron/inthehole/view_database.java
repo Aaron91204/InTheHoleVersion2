991b6ee92ea093b61578f342e0820f082d8e6001
Aaron Nicholl <aaronnicholl@ymail.com>
2018-04-06 12:19:45 +0100

    New updates 4th April

--- app/src/main/java/com/example/aaron/inthehole/view_database.java
+++ app/src/main/java/com/example/aaron/inthehole/view_database.java
@@ -71,9 +71,12 @@ public class view_database extends AppCompatActivity {
         ref.child(userid).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                showData(dataSnapshot);
+                if (dataSnapshot.exists()) {
+                    showData(dataSnapshot);
+                } else {
+                    finish();
+                    Toast.makeText(view_database.this, "Please input your details", Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
